e9078786ab83add24b81a813612df4718b88a7e0
Sagarika Shinde <sagarikashinde99@gmail.com>
2021-08-10 22:25:07 +0530

    [sagarika]: Harmonic Number Program Added

---
+++ src/com/bridgelabz/basicprograms/HarmonicNum.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.basicprograms;
+
+import java.util.Scanner;
+
+public class HarmonicNum {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Harmonic Value of N: ");
+		int HarmonicN = sc.nextInt();
+		double result = 0.0;
+		
+			for (int i=1; i<=HarmonicN; i++) {
+				result = result + (double)1/i;
+				System.out.println(+result);
+		}
+		
+	}
+
+}
