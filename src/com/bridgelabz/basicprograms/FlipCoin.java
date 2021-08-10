2f0f52f31765d029d11af442fbfcc4fcfdda0eba
Sagarika Shinde <sagarikashinde99@gmail.com>
2021-08-10 17:51:15 +0530

    [sagarika]: Leap Year Program Added

--- src/com/bridgelabz/basicprograms/FlipCoin.java
+++ src/com/bridgelabz/basicprograms/FlipCoin.java
@@ -1,5 +1,30 @@
 package com.bridgelabz.basicprograms;
 
-public class FlipCoin {
+import java.util.Scanner;
 
+public class FlipCoin {
+	private static final double Random_check = 0.5;
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter how many times to flip coin: ");
+		int NoOfFlips = scanner.nextInt();
+		int heads=0;
+		int tails=0;
+		
+		for (int i=0; i<NoOfFlips; i++) {
+			double randNum = Math.random();
+			if (randNum<Random_check) {
+				heads++;
+			}
+			else {
+				tails++;
+			}
+		}
+		double headsPercentage = (heads*100)/NoOfFlips;
+		double tailsPercentage = (tails*100)/NoOfFlips;
+		System.out.println(+headsPercentage);
+		System.out.println(+tailsPercentage);
+		scanner.close();
+	}
 }
