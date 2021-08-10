470ec1d1e897a721d56187d67154d721aed4877b
Sagarika Shinde <sagarikashinde99@gmail.com>
2021-08-10 23:43:46 +0530

    [sagarika]: Prime Factors Program Added

--- src/com/bridgelabz/basicprograms/Factors.java
+++ src/com/bridgelabz/basicprograms/Factors.java
@@ -4,7 +4,19 @@ import java.util.Scanner;
 
 public class Factors {
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Value of N: ");
+		int Num = sc.nextInt();
+		  
+	      for(int i = 2; i<= Num; i++) {
+	         while(Num%i == 0) {
+	            System.out.println(i+" ");
+	            Num = Num/i;
+	         }
+	      }
+	    //  if(Num >2) {
+	   // 	  System.out.println(Num);
+	   //   }
 	}
 
 }
