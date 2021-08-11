f97f0c1f1bcf23205ce53cf6a612de3c0e15957c
Sagarika Shinde <sagarikashinde99@gmail.com>
2021-08-11 18:45:35 +0530

    [sagarika]:Perfect Number Program Added

---
+++ src/com/bridgelabz/logicalprograms/PerfectNum.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.logicalprograms;
+
+import java.util.Scanner;
+
+public class PerfectNum {
+	public static void main(String[] args) {
+		int sum = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter an integer:");
+		int num = sc.nextInt();
+		
+		for(int i = 1; i < num; i++){
+			if(num % i == 0){
+				sum = sum + i;
+			}
+		}
+		if(sum == num){
+			System.out.println("it is a Perfect number");
+		}
+		else{
+			System.out.println("it is not a Perfect number");
+		}    
+	}
+}
